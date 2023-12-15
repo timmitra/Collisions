d30dae894229c32cff2cea438a30a1f84d6d51aa
Tim Mitra <tim@it-guy.com>
2023-12-14 19:50:38 -0500

    Initial Commit

---
+++ Collisions/CollisionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollisionsApp.swift
+//  Collisions
+//
+//  Created by Tim Mitra on 2023-12-14.
+//
+
+import SwiftUI
+
+@main
+struct CollisionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
